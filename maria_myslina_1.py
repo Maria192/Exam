797df0e5e1bbf5fa8cedc3245cdd9fd5f1b4c46d
Maria192 <maria.myslina@gmail.com>
2016-06-28 20:52:50 +0300

    test

---
+++ maria_myslina_1.py
@@ -0,0 +1,8 @@
+a = float(input('Enter the 1st number '))
+b = float(input('Enter the 2nd number '))
+c = float(input('Enter the 3rd number '))
+
+if (a / b) == c:
+    print ('Yes')
+else:
+    print ('No')
