3a384722b42eb8f8a8d7b6b7925739512c6f3907
Maria192 <maria.myslina@gmail.com>
2016-06-29 11:21:39 +0300

    itog

--- exam.py
+++ exam.py
@@ -1,27 +1,62 @@
 import re
+import os
 
 def write (d):
     fw = open('written.txt','w', encoding = 'UTF-8')
     for wrd in d:
         fw.write(wrd + '\n')
     fw.close()
+    
+def create(d):
+    for key in d:
+        os.mkdir(key)
+        dire = os.getcwd()
+        
+        dire = dire + '\\' + key
+        print (dire)
+        fw = open(dire + key, 'w', encoding = 'UTF-8')
 
 def words(arr):
     words = []
-    i = 1
-    lex = '(?:(?:(?: [А-Я]\.){1,2}) [А-Я][а-я].+?[ ,.<"-])|(?: [А-Я][а-я]+ [А-Я][а-я].+?[ ,.<"-])'
+    name = ' [А-ЯЁ]\. [А-ЯЁ][а-яё].+?[ ,.<"\-&;:]'
+    res = re.findall(name, arr)
+    for wrd in res:
+        if 'Примечания' in wrd:
+            break
+        else:
+            wrd = wrd[1:-1]
+            words.append(wrd)
+    for wrd in words:
+        print(wrd)
+    print('\n')
+   
+
+def names (arr):
+    words = []
+    d = {}
+    lex = '(?:(?:(?: [А-ЯЁ]\.){2,2}) [А-ЯЁ][а-яё].+?[ ,.<"\-&:;])|(?:(?: [А-ЯЁ][а-яё]+){1,2} [А-ЯЁ][а-яё].+?[ ,.<"\-&:;])'
     res = re.findall(lex, arr)
     for wrd in res:
-        wrd = wrd[1:-1]
-        words.append(wrd)
-    write (words)
-
+        if 'Примечания' in wrd:
+            break
+        else:
+            wrd = wrd[1:-1]
+            words.append(wrd)
+            fami = re.findall('.* ([А-ЯЁ][а-яё]+)',wrd)
+            for elem in fami:
+                d[elem] = 'Предложение'
+    for wrd in words:
+        print(wrd)
+    create (d)
+    
 
 def openfile (name):
     f = open(name,'r', encoding = 'UTF-8')
     s = f.read()
     f.close()
-    words (s)          
+    #print (s)
+    words(s)
+    names(s)
     
 
 def main():
